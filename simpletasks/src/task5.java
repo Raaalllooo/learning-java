936f80c4aa82f60e68fa4864fad2f3512534fc63
Sirniy <jackwolfthrone@gmail.com>
2019-11-07 11:29:10 +0200

    int -> double

--- simpletasks/src/task5.java
+++ simpletasks/src/task5.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
-class minutestoyears {
+class task5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println ("Input your minutes value");
         int minutes = sc.nextInt();
         int numberOfMinutes = minutes;
-        //System.out.println (numberOfMinutes);
+        System.out.println (numberOfMinutes);
         int days = numberOfMinutes / 1440;
-        //System.out.println (days);
+        System.out.println (days);
         int years = days / 365;
         int daysModulo = days % 365;
         //System.out.println (daysModulo);
