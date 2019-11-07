da057489ae724030b8a82ef339bf9b4380053a87
Sirniy <jackwolfthrone@gmail.com>
2019-11-07 17:55:45 +0200

    брух

---
+++ simpletasks/src/task7.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class task7 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String firstStr = input.nextLine().toLowerCase();
+        int[] array = new int[firstStr.length()];
+
+    }
+}
