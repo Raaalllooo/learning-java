ab09181fad6b2458aa516001c890b77db5777651
Sirniy <jackwolfthrone@gmail.com>
2019-11-07 14:13:16 +0200

    the quick brown fox и queen удалить совпадения без коментов

--- simpletasks/src/task6.java
+++ simpletasks/src/task6.java
@@ -1,4 +1,3 @@
-//import java.util.Arrays;
 import java.util.Scanner;
 
 public class task6 {
@@ -6,28 +5,13 @@ public class task6 {
         Scanner input = new Scanner(System.in);
         System.out.println ("Input your first string:");
         String firstStr = input.nextLine();
-        //char []firstCh = firstStr.toCharArray();
         System.out.println ("Input your second string:");
         String secondStr = input.nextLine();
         char []secondCh = secondStr.toCharArray();
-        //char []letters = new char[256];
         String finalString = firstStr;
-        //System.out.println (Arrays.toString(firstCh));
-        //System.out.println (Arrays.toString(secondCh));
-        /*
-        for (int i = 0; i<secondStr.length(); i++){
-            letters[secondStr.charAt(i)]++;
-        }
-        System.out.println("\nThe new string is: ");
-        for (int i = 0; i<firstStr.length(); i++){
-            if (letters[firstStr.charAt(i)] == 0)
-                System.out.print(firstStr.charAt(i));
-        }
-         */
         for (int i=0; i<secondStr.length(); i++){
             finalString = finalString.replaceAll(String.valueOf(secondCh[i]),"");
         }
-        //finalString = finalString.replaceAll("\\s+","");
         System.out.println(finalString);
     }
 }
